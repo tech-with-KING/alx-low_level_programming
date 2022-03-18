da0e5737dc56b4af269d7e958113a4cc29e13a97
Okpo Kingsley Francis <kingsleyfrancis42@gmail.com>
2022-03-18 08:53:19 +0100

    'lool'

--- 0x04.c/0-main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "0-main.h"
-
-/**
- * main - Prints numbers between 00 to 99.
- *
- * Return: Always 0 (Success)
- */
--- 0x04.c/main.h
+++
@@ -1,2 +0,0 @@
-#include "main.h"
-#include "0x01-variables_if_else_while/0-positive_or_negative"
