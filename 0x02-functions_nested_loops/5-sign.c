e59fea6c91ff8f1793ba9a2a375333fc5537a436
cosmic <tech-with-KING>
2022-06-07 17:39:38 -0400

    checksign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+/**
+ *print_sign - this checks if the supplied parameter is lower case
+ *@c: this is the value to be usee for the test
+ *Description: print_sign return 1 if the value is positive ans return0 if 
+ *
+ *Return: 1 if the function is lower else return 0
+ */
+int print_sign(int c)
+{
+	int n;
+
+	n = c;
+	if (n > 0)
+	{
+		printf("+\n");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-\n");
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		printf("0\n");
+		return (0);
+			
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _putchar(int c);
 void print_alphabet_x10();
 int _islower(int c);
 int _isalpha(int c);
+int print_sign(int c);
