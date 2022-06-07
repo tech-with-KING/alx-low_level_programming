aa9b8d7b652a342dce4fee61854fea7f2d6497b9
cosmic <tech-with-KING>
2022-06-07 15:15:35 -0400

    alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - this prints 26 alphabet
+ *
+ *Description: print_alphabet_x10 prints the english alphabet
+ *
+ *Return: return void
+ */
+void print_alphabet_x10(void)
+{
+	int j = 'a';
+	int i = 0;
+
+	while (i <= 10)
+	{
+		while (j <= 'Z')
+		{
+			putchar(j);
+		}
+		putchar('\n');
+		i++;
+	}
+
+}
