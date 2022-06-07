b33578106b0f33f8b6ba2f447d577839854a34f6
cosmic <tech-with-KING>
2022-06-07 17:26:03 -0400

    is

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 #include <ctype.h>
 #include <stdint.h>
 /**
- *_islower - this checks if the supplied parameter is lower case
+ *_isalpha - this checks if the supplied parameter is lower case
  *@c: this is the value to be usee for the test
- *Description: _islower prints the english alphabet
+ *Description: _isalpha prints the english alphabet
  *
  *Return: 1 if the function is lower else return 0
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	int n;
 
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,3 +5,4 @@ void print_alphabet();
 int _putchar(int c);
 void print_alphabet_x10();
 int _islower(int c);
+int _isalpha(int c);
