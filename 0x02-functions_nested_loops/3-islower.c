27dc95e27bd863f237b6bec49228b0511df22505
cosmic <tech-with-KING>
2022-06-07 17:19:34 -0400

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 #include <stdint.h>
 /**
  *_islower - this checks if the supplied parameter is lower case
- *
+ *@c: this is the value to be usee for the test
  *Description: _islower prints the english alphabet
  *
  *Return: 1 if the function is lower else return 0
