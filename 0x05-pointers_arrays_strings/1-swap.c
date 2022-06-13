990466f5342e651f3e83258f88a74b0a2abaf524
cosmic <tech-with-KING>
2022-06-13 10:53:21 -0400

    swap int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - this function prins a ponter to a value;
+ *
+ *@b: this parameter is the needed parameter
+ *@a: this parameter is the second value to be printed
+ */
+
+void swap_int(int a, int b)
+{
+	int *p, *r;
+
+	p = a;
+	r = b;
+	a = r;
+	b = p;
+}
