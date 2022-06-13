77965d81d65a76b98cbe6ffd9eb786c44274fd44
cosmic <tech-with-KING>
2022-06-13 15:06:15 -0400

    puts a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_puts - prints a character to the stdio
+ *
+ *@str: this is going to be the supplied argument
+ */
+void _puts(char *str)
+{
+	const char *word;
+
+	strcpy(*str, word);
+	puts(word);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@
 void reset_to_98(int *n);
 void swap_int(int *x, int *y);
 int _strlen(char *s);
+void _puts(char *str);
