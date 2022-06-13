780d9c97deb822c30de6564695a2e90021544ebf
cosmic <tech-with-KING>
2022-06-13 14:32:28 -0400

    this function returns string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strlen - this function gives the length of any string passed to it
+ *
+ *Return: 0 always succes
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = _strlen(*s);
+	return (len);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
 #include <stdio.h>
 void reset_to_98(int *n);
-void swap_int(int *x , int *y);
+void swap_int(int *x, int *y);
+int _strlen(char *s);
