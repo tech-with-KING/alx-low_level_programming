68f996a6098015419f2a853dc19474114c9a9fed
cosmic <tech-with-KING>
2022-06-13 10:11:55 -0400

    the address of the integer is going to be reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *reset_to_98 - this function prins a ponter to a value;
+ *
+ *@n: this parameter is the needed parameter
+ */
+
+void reset_to_98(int *n)
+{
+	int a;
+
+	a = 98;
+	*n = a;
+}
