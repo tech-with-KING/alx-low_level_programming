ddc8fd6f156753692f9d9c02481e07bc78d4694c
cosmic <tech-with-KING>
2022-06-15 19:03:13 -0400

    string conncatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_strcat - this fuction concatenates two string
+ *
+ *Return: age this is th brand new pair
+ *
+ *@dest: dest is the first param
+ *@src: src is the second parameter
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = strlen(dest);
+	char *age;
+
+	while (dest[i] != '\n')
+	{
+		age[i] = dest[i];
+		i++;
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		age[j] = src[i];
+		j++, i++;
+	}
+	printf("%s", age);
+	return (age);
+}
