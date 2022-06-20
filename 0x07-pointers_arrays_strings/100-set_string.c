69dbea0013734fcd34c31c0120d01b2ba109340f
cosmic <tech-with-KING>
2022-06-20 11:36:36 -0400

    doing a good job

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *set_sring - thid function sets the value of a string
+ *set_string - thid function sets the value of a string
  *Return: void
  *@s: this parameter is the ponter parameter that is goin to be pointing
  *@to: this is the character arrays that is goin to be pointed to
@@ -8,5 +8,5 @@
 
 void set_string(char **s, char *to)
 {
-	*s =to;
+	*s = to;
 }
