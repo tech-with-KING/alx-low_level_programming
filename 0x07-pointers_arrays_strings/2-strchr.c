96b45fa998f51641ee9dbcbc00eb3a17161a2edf
cosmic <tech-with-KING>
2022-06-21 19:30:10 -0400

    memset

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,15 @@
  *_strchr - thid function loops through a string to find a char
  *@s: string to be checked
  *@c: checked string
- *Return: c or NULL
+ *Return: NULL if pointer is found or 
  */
 
 char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while ( *s != '\0')
 	{
 		if (*(s + i) == c)
 		{
@@ -24,6 +25,8 @@ char *_strchr(char *s, char c)
 			return (s + i);
 		}
 		return (s + i);
+		i++;
 	}
 
+	return (s + 1);
 }
