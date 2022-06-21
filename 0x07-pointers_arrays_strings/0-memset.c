ef7a1c4a7d93396bfdabd85bd0efe9eec178db63
cosmic <tech-with-KING>
2022-06-21 18:40:45 -0400

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,67 +8,18 @@
  *@n: this parameter is the length value
  *Return:value
  */
-
-int main(void)
+char *_memset(char *value, char b, unsigned int n)
 {
-	char (*a)[8];
+	char *boll;
+	int i;
 
-	int i ;
-	int j;
-	j =0;
 	i = 0;
-	char p[8][8] ={{'b','b','c','d','e',' ','d','k'},
-	{'a','b','c','d','e','f','p','k'},{'a','b','c','d','e','f','d','k'},{'a','b','c','d','e','f','d','k'},
-	{'l','b','c','d','e','f','l','k'},{'a','b','c','d','e','f','d','k'},{'a','b','c','d','e','f','d','k'},
-	{'a','b','c','d','e','l','d','k'}
-	};
-	
-	for ( ; i < 8;i++)
+	while (i < n)
 	{
-		a =*(p+i);
-		for ( ; j < 8; j++)
-		{
-			printf(" %c\n",(*a))[j];
-		}	
+		boll = &b;
+		value[i] = *boll;
+		i++;
 	}
-	
-	
-	return(0);
-
+	printf("%s\n", value);
+	return (value);
 }
-
-
-// char *_strchr(char *s, char c)
-// {
-// 	int i;
-// 	for ( i = 0; s[i] !='\0'; i++)
-// 	{
-// 		if (s[i]==c)
-// 		{
-// 			return(s[i]);
-// 			break;
-			
-// 		}
-// 		else
-// 		{
-// 			return(NULL);
-
-// 		}
-// 	}
-// 	return s;
-// }
-
-
-// char *_memset(char *value, char b, unsigned int n)
-// {
-// 	char *boll;
-// 	int i = 0;
-// 	while (i < n)
-// 	{
-// 		boll = &b;
-// 		value[i] = *boll;
-// 		i++;
-// 	}
-// 	printf("%s\n",value);
-// 	return (value);
-// }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,9 @@
 
+
 #include <stdio.h>
 #include <string.h>
 void set_string(char **s, char *to);
 void print_diagsum(int *a, int sum);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *value, char b, unsigned int n);
 char *_strchr(char* s, char c);
