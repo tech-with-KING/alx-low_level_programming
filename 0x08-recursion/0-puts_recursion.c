6bafd5fc568459986e1be913aa7d1b9530a04701
cosmic <tech-with-KING>
2022-06-22 11:41:11 -0400

    reccurssive printing

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,12 @@
 /**
  *_puts_recursion - this prints a string to stdo with a new line
  *
- *@@s: this is the string to be printed out
+ *@s: this is the string to be printed out
  *Return: void
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	i = 0;
-	printf("%s\n",s);
+	if (*s == '\0')
+		printf("%c", *s);
+	_puts_recursion(s + 1);
 }
