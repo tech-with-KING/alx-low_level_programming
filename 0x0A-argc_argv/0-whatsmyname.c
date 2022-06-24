599d63419e714168c0c6f89766eca0e8dd48476b
cosmic <tech-with-KING>
2022-06-24 16:58:27 -0400

    iyalaya yin

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *@argc: this parameter is the argument noo
  *@argv: this parameter is the argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[0])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
