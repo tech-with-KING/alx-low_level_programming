b66804154e017e74fdbb3ba4249bdc25899445cf
cosmic <tech-with-KING>
2022-07-04 10:03:58 -0400

    the macro defination worked well

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
+#ifndef NOBS
+#define NOBS
 #define ABS(X) ((X) >= 0 ? (X) : (X) * -1)
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef SUM_MACRO
+#define SUM_MACRO
 #define SUM(A, B) ((A) + (B))
 #endif
