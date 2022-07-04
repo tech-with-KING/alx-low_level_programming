1302281e39e976ffb15bcffe0ef34d8ae456e374
cosmic <tech-with-KING>
2022-07-04 13:46:44 -0400

    structured arrangement is still the one perfection imortals retain

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_STRU
+#define DOG_STRU
+/**
+ *struct dog - this contains the dog properties
+ *@name: char
+ *@age: float
+ *@owner: char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
