530923c39a4fe32ba176bb5478ab76403550e949
cosmic <tech-with-KING>
2022-07-04 14:08:31 -0400

    dog strucc

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ *init_dog - this fuction initializes the parameters of dog
+ *@name: this is gong to be the passed name for the dog
+ *@age: this is  the value  for the dog struct age propertie
+ *@owner: this char is the name of the owner
+ *@d: this is the dog to be named
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog;
+
+	dog.age = age;
+	dog.name = name;
+	dog.owner = owner;
+}
