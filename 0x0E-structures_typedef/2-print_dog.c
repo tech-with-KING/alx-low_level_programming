234f081ebc006f29570752277858fdf10bd5b144
cosmic <tech-with-KING>
2022-07-04 17:46:20 -0400

    upward

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,8 +8,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		d->name == NULL ? printf("nill\n") : printf("Name: %s\n", d->name);
-		d->age == '\0' ? printf("nill\n") : printf("Age: %f\n", d->age);
-		d->owner == NULL ? printf("nill\n") : printf("Owner: %s\n", d->owner);
+		d->name == NULL ? printf("Name: (nill)\n") : printf("Name: %s\n", d->name);
+		d->age == '\0' ? printf("Age: (nill)\n") : printf("Age: %f\n", d->age);
+		d->owner == NULL ? printf("Owner: nill\n") : printf("Owner: %s\n", d->owner);
 	}
 }
