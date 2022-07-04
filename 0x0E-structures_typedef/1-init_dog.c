8199944782ea1601cb5e842f46c1690eec5571e6
cosmic <tech-with-KING>
2022-07-04 15:56:19 -0400

    sometimes your dogs bite is all you need to make your day

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,9 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog dog;
+	d ->name = name;
+	d ->age = age;
+	d ->owner = owner;
 
-	dog.age = age;
-	dog.name = name;
-	dog.owner = owner;
 }
