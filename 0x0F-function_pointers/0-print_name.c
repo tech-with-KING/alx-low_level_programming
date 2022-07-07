0d828d3c23641928654005977baa28e65d4e1ff9
cosmic <tech-with-KING>
2022-07-07 06:43:12 -0400

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "functions.h"
+#include <stdio.h>
+/**
+ *print_name - this function calls the print name function
+ *Retunrs: void
+ *@name: this is the  name to be printed out
+ *@f:this function is called passing the parameter name to it 
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This file describes the pointers to a functions and the functions can be called 
