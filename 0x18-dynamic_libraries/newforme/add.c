23fa4ee1edb32a2f0a908f16540902b089921e96
cosmic <kingsleyfrancis42@gmail.com>
2022-09-05 18:20:56 -0400

    more is better

---
+++ 0x18-dynamic_libraries/newforme/add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *add - this function check if a given character given is upper case
+ *Return: sum
+ *@a: this is the character given in integer form
+ *@b: this is the character given in integer form
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
