ed6c1ade34736a8f164f00eb6c7362d555ac268c
cosmic <kingsleyfrancis42@gmail.com>
2022-09-30 14:06:08 +0100

    moralle

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+/**
+ * list_len - print all elements of a `list_t` list
+ * @h: linked list head
+ * Return: number of nodes
+ */
 size_t list_len(const list_t *h)
 {
 	const list_t *c;
 	size_t count;
-	
+
 	for (c = h, count = 0; c != NULL;  c = c->next)
 	{
 		count++;
