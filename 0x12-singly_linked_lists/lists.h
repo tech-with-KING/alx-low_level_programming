26cc0f8134d60e10ce58d266694a09d21e681971
cosmic <kingsleyfrancis42@gmail.com>
2022-09-30 12:46:08 +0100

    nath dave

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,7 +16,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
-size_t listint_len(const list_t *h);
+size_t list_len(const list_t *h);
 list_t *add_nodeint(list_t **head, const int n);
 list_t *add_nodeint_end(list_t **head, const int n);
 void free_listint(list_t *head);
