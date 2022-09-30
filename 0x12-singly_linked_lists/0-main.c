e64b89f304bfe6cf0d8db6de07568c098dde8d50
cosmic <kingsleyfrancis42@gmail.com>
2022-09-30 13:58:12 +0100

    i made it happen

--- 0x12-singly_linked_lists/0-main.c
+++ 0x12-singly_linked_lists/0-main.c
@@ -26,9 +26,15 @@ int main(void)
     new->len = 5;
     new->next = head;
     head = new;
-    n = list_len(head);
+    n = print_list(head);
     printf("-> %lu elements\n", n);
+
+    printf("\n");
     free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+
     free(new);
     return (0);
 }
--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,23 +12,18 @@ size_t print_list(const list_t *h)
 {
 	size_t count;
 	const list_t *c;
-	
-	for (c = h, count = 0; c != NULL;  c = c->next ,count++)
+
+	for (c = h, count = 0; c != NULL;  c = c->next, count++)
 	{
-		if (c-> str == NULL || c->len == '\0')
+		if (c->str == NULL || c->len == '\0')
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("[%d] %s\n",c->len, c->str);
+			printf("[%d] %s\n", c->len, c->str);
 		}
 
 	}
 	return (count);
 }
-
-
-	
-			       
-
