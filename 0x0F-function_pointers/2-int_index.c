703c4a922a5467103b5b1efa49acc0ee47a59f06
cosmic <kingsleyfrancis42@gmail.com>
2023-03-22 17:53:51 +0100

    0-print_name: this is a simple fuction that calls a function passed to it using the function parameter

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,2 +1,7 @@
-#include "main.h"
+#include "function_pointers.h"
 #include <stdio.h>
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	return (size);
+}
+
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef HEAD_H
 #define HEAD_H
+#include <stddef.h>
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array,  size_t, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 #endif
